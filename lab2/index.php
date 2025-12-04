38db87c133a607f6e183df9712b8978c2a27f88c
Kunano55 <kukongkaka@gmail.com>
2025-12-04 09:50:17 +0700

    Update index.php

--- lab2/index.php
+++ lab2/index.php
@@ -14,10 +14,38 @@
     <h1>Associative Arrays</h1>
     <?php
         $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43", "Kong"=>"21");
-        echo "Peter is " . $age['Peter'] . " years old."."<br>";
-        echo "Ben is " . $age['Ben'] . " years old."."<br>";
-        echo "Joe is " . $age['Joe'] . " years old."."<br>";
-        echo "Kong is " . $age['Kong'] . " years old."."<br>";
+        echo "Peter is " . $age['Peter'] . " years old."."<br>\n";
+        echo "Ben is " . $age['Ben'] . " years old."."<br>\n";
+        echo "Joe is " . $age['Joe'] . " years old."."<br>\n";
+        echo "Kong is " . $age['Kong'] . " years old."."<br>\n";
+    ?>
+    <h1>How to use For with Index Arrays</h1>
+    <?php
+        $fluits = array("Banana", "Apple", "Orange", "Mango", "Watermelon", "Pineapple", "Grapes", "Strawberry");
+        $arratLengeth = count($fluits);
+
+        for($x = 0; $x < $arratLengeth; $x++) {
+            echo $fluits[$x];
+            echo "<br>\n";
+        }
+    ?>
+    <h1>How to use Foreach with Index Arrays</h1>
+    <?php
+        $fluits = array("Banana", "Apple", "Orange", "Mango", "Watermelon", "Pineapple", "Grapes", "Strawberry");
+
+        foreach($fluits as $value) {
+            echo $value;
+            echo "<br>\n";
+        }
+    ?>
+    <h1>How to use Foreach with Associative Arrays</h1>
+    <?php
+        $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43", "Kong"=>"21");
+        
+        foreach($age as $x => $x_value) {
+            echo "Key=" . $x . ", Value=" . $x_value;
+            echo "<br>\n";
+        }
     ?>
 </body>
 </html>
