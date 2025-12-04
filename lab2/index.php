e7c6dc21562ab7785f81ddb50fa124f4f612bc2e
Kunano55 <kukongkaka@gmail.com>
2025-12-04 09:10:38 +0700

    Add files via upload

---
+++ lab2/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php array 12/04/2025</title>
+</head>
+<body>
+    <?php
+        $car = array("Volvo", "BMW", "Toyota");
+        echo "I like " . $car[0] . ", " . $car[1] . " and " . $car[2] . ".";
+    ?>
+</body>
+</html>
